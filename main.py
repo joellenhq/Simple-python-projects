45f5179b79082f421c7e5a1e0e3f3f29e03c2df2
joellenhq <73105482+joellenhq@users.noreply.github.com>
2022-04-05 01:23:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from arithmetic_arranger import arithmetic_arranger
 from time_calculator import add_time
+import budget
+from budget import create_spend_chart
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
@@ -7,3 +9,22 @@ print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 print(add_time("11:59 PM", "24:05"))
 print(add_time("11:06 PM", "2:02"))
 print(add_time("3:30 PM", "2:12"))
+
+food = budget.Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = budget.Category("Clothing")
+#food.print_obj()
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = budget.Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
+print(food)
+print(clothing)
+
+print(create_spend_chart([food, clothing, auto]))
